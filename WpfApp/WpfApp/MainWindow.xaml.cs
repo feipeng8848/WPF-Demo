f860793c75ed835345aaaf8ea48c3dab7412723b
wangzk <wangzk@suooter.com>
2019-10-09 11:17:13 +0800

    添加源代码

---
+++ WpfApp/WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using WpfApp.ViewModel;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var dataContext = new MainViewModel() {
+                Name = "FeiPeng8848"
+            };
+            DataContext = dataContext;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainViewModel vm = DataContext as MainViewModel;
+            vm.Name = "haha";
+        }
+    }
+}
---
+++ WpfApp/WpfApp/ViewModel/MainViewModel.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApp.ViewModel
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        #region INPC
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        #endregion
+
+
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        public ICommand ChangeName
+        {
+            get
+            {
+                return new RelayCommand() {
+                    CanExecutePredicate = a => true,
+                    ExecuteAction = a =>
+                    {
+                        Name = "ChangeName Command";
+                    }
+                };
+            }
+        }
+    }
+
+    /// <summary>
+    /// 路由命令
+    /// </summary>
+    internal class RelayCommand : ICommand
+    {
+        public Predicate<object> CanExecutePredicate { get; set; }
+        public Action<object> ExecuteAction { get; set; }
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="canExecute"></param>
+        /// <param name="execute"></param>
+        public RelayCommand(Predicate<object> canExecute, Action<object> execute)
+        {
+            CanExecutePredicate = canExecute;
+            ExecuteAction = execute;
+        }
+        /// <summary>
+        /// ICommand字段
+        /// </summary>
+        public RelayCommand()
+        { }
+        /// <summary>
+        /// ICommand字段
+        /// </summary>
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+        /// <summary>
+        /// ICommad字段
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
+        public bool CanExecute(object parameter)
+        {
+            return CanExecutePredicate == null || CanExecutePredicate(parameter);
+        }
+        /// <summary>
+        /// ICommand字段
+        /// </summary>
+        /// <param name="parameter"></param>
+        public void Execute(object parameter)
+        {
+            ExecuteAction(parameter);
+        }
+    }
+}
